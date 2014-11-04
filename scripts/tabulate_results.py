070373f884f0a16f44dcb2ec2c4a04be9751ba63
Stephen <brawner@gmail.com>
2014-11-04 12:32:57 -0500

    p success

--- scripts/tabulate_results.py
+++ scripts/tabulate_results.py
@@ -13,7 +13,7 @@ def calculate_interval(num_successes, num_trials):
 	num_smoothed_success = (num_successes + 0.5 * z2) / num_smoothed_trials
 	num_smoothed_failure = 1.0 - num_smoothed_success
 	interval = z * math.sqrt(num_smoothed_success * num_smoothed_failure / num_smoothed_trials)
-	return interval
+	return num_smoothed_success, interval
 
 if len(argv) > 1:
 	directory = argv[1]
@@ -43,6 +43,5 @@ if len(argv) > 1:
 	print("Depth, Depth Type, Successes, Estimate Successes, Informed Guesses, Total Trials")
 	for depth_type, data_by_depth in data.iteritems():
 		for depth, data_sum in data_by_depth.iteritems():
-			probability_success = data_sum[0] / data_sum[3]
-			interval = calculate_interval(data_sum[0], data_sum[3])
+			probability_success, interval = calculate_interval(data_sum[0], data_sum[3])
 			print(str(depth) + ", " + str(depth_type	) + ", " + str(data_sum) + ", " + probability_success + " +- " + str(interval))
