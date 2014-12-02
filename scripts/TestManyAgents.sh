05d43bbe63c2ce2547c281275b3f98338ea6f8c8
Stephen Brawner <brawner@cs.brown.edu>
2014-12-02 14:02:12 -0500

    fixing

--- scripts/TestManyAgents.sh
+++ scripts/TestManyAgents.sh
@@ -8,5 +8,5 @@ ERR_DIRECTORY="$HOME/errors/many_agents_${JOB_ID:-$TIMESTAMP}"
 #echo $DIRECTORY
 mkdir -p $DIRECTORY
 mkdir -p $ERR_DIRECTORY
-java -Xms1g -Xmx4g -cp ~/workspace/baking/h2r-baking.jar edu.brown.cs.h2r.baking.Experiments.TestManyAgents > 2>"$ERR_DIRECTORY/$SGE_TASK_ID.err" 1>"$DIRECTORY/$SGE_TASK_ID.csv"
+java -Xms1g -Xmx4g -cp ~/workspace/baking/h2r-baking.jar edu.brown.cs.h2r.baking.Experiments.TestManyAgents 2>"$ERR_DIRECTORY/$SGE_TASK_ID.err" 1>"$DIRECTORY/$SGE_TASK_ID.csv"
 
