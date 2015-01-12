e7f571c077aa49d6a921a6f9d933af773d82eb9a
Stephen <brawner@gmail.com>
2015-01-12 11:55:43 -0500

    updating script

--- scripts/tabulate_results_manyAgents.py
+++ scripts/tabulate_results_manyAgents.py
@@ -6,37 +6,38 @@ import numpy
 
 
 if len(argv) > 1:
-    directory = argv[1]
-    files = glob(directory + "/*.csv")
-    data = dict()
-    for filename in files:
-        print("Processing file " + filename)
-        file = open(filename, 'rb')
-        
-        csvreader = reader(file, delimiter=",")
-        try:
-            isFirst = True
-            data_lines = []
-            for line in csvreader:
-                if line:
-                    if isFirst:
-                        labels = line
-                        isFirst = False
-                    else:
-                        data_lines.append(line)
-        except:
-            print("error on file")
-            continue
+    directories = argv[1:]
+    for directory in directories:
+        files = glob(directory + "/*.csv")
+        data = dict()
+        for filename in files:
+            print("Processing file " + filename)
+            file = open(filename, 'rb')
+            
+            csvreader = reader(file, delimiter=",")
+            try:
+                isFirst = True
+                data_lines = []
+                for line in csvreader:
+                    if line:
+                        if isFirst:
+                            labels = line
+                            isFirst = False
+                        else:
+                            data_lines.append(line)
+            except:
+                print("error on file")
+                continue
 
-        #Agent, Successes, Trials, Average reward, average successful reward
-        for line in data_lines:
-            if len(line) != 5:
-		continue
-            agent = line[0]
-            if agent not in data.keys():
-                data[agent] = [[],[],[],[]]
-            for i in range(4):
-                data[agent][i].append(float(line[i+1]))
+            #Agent, Successes, Trials, Average reward, average successful reward
+            for line in data_lines:
+                if len(line) != 5:
+                    continue
+                agent = line[0]
+                if agent not in data.keys():
+                    data[agent] = [[],[],[],[]]
+                for i in range(4):
+                    data[agent][i].append(float(line[i+1]))
 
     print("Agent, Successes, Trials, Average reward, average successful reward")
     for agent, line in data.iteritems():
