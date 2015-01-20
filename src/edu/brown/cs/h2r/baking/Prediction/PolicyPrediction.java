a46bf15036bcbc69cfe1c9191663a4c79444c36e
Stephen <brawner@gmail.com>
2015-01-20 12:18:58 -0500

    parallel

--- src/edu/brown/cs/h2r/baking/Prediction/PolicyPrediction.java
+++ src/edu/brown/cs/h2r/baking/Prediction/PolicyPrediction.java
@@ -223,6 +223,7 @@ public class PolicyPrediction {
 				new PolicyProbabilityCallable(this.policies, fromStateTuple, goalCondition, maxDepth, 
 						actualName, hashFactory, depthType);
 		
+		/*
 		List<PolicyProbability> result = new ArrayList<PolicyProbability>();
 		for(KitchenSubdomain policy : this.policies) {
 			PolicyProbability prob;
@@ -235,10 +236,10 @@ public class PolicyPrediction {
 				e.printStackTrace();
 			}
 			
-		}
+		}*/
 		
 		
-		//List<PolicyProbability> result = Parallel.ForEach(this.policies, (ForEachCallable<KitchenSubdomain, PolicyProbability>)runnable);
+		List<PolicyProbability> result = Parallel.ForEach(this.policies, (ForEachCallable<KitchenSubdomain, PolicyProbability>)runnable);
 		
 		double sumProbability = 0.0;
 		for (PolicyProbability policyProb : result) {
