a40771741832bd5f8b7c8e9c31e9799dc90bee32
Stephen <brawner@gmail.com>
2015-02-02 13:27:37 -0500

    scripts

--- scripts/tabulate_results_manyAgents.py
+++ scripts/tabulate_results_manyAgents.py
@@ -47,19 +47,26 @@ if len(argv) > 1:
         results.append([agent, int(sum(line[0])), int(sum(line[1])), numpy.mean(line[2]), 1.96 * numpy.std(line[2], ddof=1)/math.sqrt(len(line[2])), numpy.mean(line[3]), 1.96 * numpy.std(line[3], ddof=1)/math.sqrt(len(line[3]))])
    
     sorted_results = sorted(results, key= lambda line: line[0])
+    
+    yCoords = "symbolic y coords={"
+    for line in sorted_results:
+        yCoords = yCoords + "{" + str(line[0]) + "},"
+    yCoords = yCoords + "}"
+    
+
     print("coordinates {")
     for line in sorted_results:
-        print("(" + str(float(line[1]) / line[2])  + ", " + str(line[0]) + ")")
+        print("(" + str(float(line[1]) / line[2])  + "," + str(line[0]) + ")")
     print("};")
 
     print("coordinates {")
     for line in sorted_results:
-        print("(" + str(line[3]) + ", " + str(line[0]) + " )\t+- (" + str(line[4]) + ", " + str(line[4]) + ")")
+        print("(" + str(line[3]) + "," + str(line[0]) + " )\t+- (" + str(line[4]) + ", " + str(line[4]) + ")")
     print("};")
 
     print("coordinates {")
     for line in sorted_results:
-        print("("  + str(line[5]) + ", " + str(line[0]) + " )\t+- (" + str(line[6]) + ", " + str(line[6]) + ")")
+        print("("  + str(line[5]) + "," + str(line[0]) + " )\t+- (" + str(line[6]) + ", " + str(line[6]) + ")")
     print("};")
 
 
