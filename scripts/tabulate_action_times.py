88e3b7322dcddee9c64540b4a02a75f14fcd6343
Stephen <brawner@gmail.com>
2015-03-12 21:45:47 -0400

    script

--- scripts/tabulate_action_times.py
+++ scripts/tabulate_action_times.py
@@ -52,5 +52,6 @@ if len(argv) > 1:
 					condensed.append([first, last])
 					first = time[0]
 				last = time[1]
+			condensed.append([first, last])
 			print(agent + "-: " + str(times))
-			print(agent + "-: " + str(condensed))
+			print(agent + "-: " + str(condensed) + "\n")
