89d7edd437e994d2aa486bbe0d1ecbea95437df5
Stephen <brawner@gmail.com>
2015-03-19 14:00:12 -0400

    scritps

--- scripts/tabulate_results_manyAgents.py
+++ scripts/tabulate_results_manyAgents.py
@@ -98,7 +98,7 @@ if len(argv) > 1:
         for agent, line in data_by_agent.iteritems():
             if agent not in results_recipes.keys():
                 results_recipes[agent] = []
-            print(str(agent) + ", " recipe + ", "  + str(int(sum(line[0]))) + ", " + str(int(sum(line[1]))) + ", " + str( numpy.mean(line[2])) + ", " + str(numpy.mean(line[3])) + " +- " + str(1.96 * numpy.std(line[3], ddof=1)/math.sqrt(len(line[3]))))
+            print(str(agent) + ", " + recipe + ", "  + str(int(sum(line[0]))) + ", " + str(int(sum(line[1]))) + ", " + str( numpy.mean(line[2])) + ", " + str(numpy.mean(line[3])) + " +- " + str(1.96 * numpy.std(line[3], ddof=1)/math.sqrt(len(line[3]))))
         
             results_recipes[agent].append([recipe, int(sum(line[0])), int(sum(line[1])), numpy.mean(line[2]), 1.96 * numpy.std(line[2], ddof=1)/math.sqrt(len(line[2])), numpy.mean(line[3]), 1.96 * numpy.std(line[3], ddof=1)/math.sqrt(len(line[3]))])
 
